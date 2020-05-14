2b04b752c84b8e4bc2e1434b9d86aa80bab87d34
Ludo <ludovic.barusseau@gmail.com>
2020-05-14 14:15:25 +0200

    Add scikit-threshold plugin

---
+++ scikit_threshold/scikit_threshold.py
@@ -0,0 +1,26 @@
+import sys
+import numpy
+import PyCore
+import PyDataProcess
+import scikit_threshold_process as processMod
+import scikit_threshold_widget as widgetMod
+
+
+# --------------------
+# - Interface class to integrate the process with Ikomia application
+# - Inherits PyDataProcess.CPluginProcessInterface from Ikomia API
+# --------------------
+class scikit_threshold(PyDataProcess.CPluginProcessInterface):
+
+    def __init__(self):
+        PyDataProcess.CPluginProcessInterface.__init__(self)
+
+
+    def getProcessFactory(self):
+        # Instantiate process object
+        return processMod.scikit_thresholdProcessFactory()
+
+
+    def getWidgetFactory(self):
+        # Instantiate associated widget object
+        return widgetMod.scikit_thresholdWidgetFactory()
