dd26368f0de2de74ced380e2df1ccabe000e1be6
Ludo <ludovic.barusseau@gmail.com>
2020-06-11 17:07:03 +0200

    Update Detectron2 requirements and path

--- Detectron2_MaskRCNN_CityScape/Detectron2_MaskRCNN_CityScape_process.py
+++ Detectron2_MaskRCNN_CityScape/Detectron2_MaskRCNN_CityScape_process.py
@@ -1,23 +1,7 @@
-import sys
-from pathlib import Path
+import update_path
 import PyCore
 import PyDataProcess
 import copy
-
-# Update Python path for pywin32 package
-if sys.platform == 'win32':
-    home_path = str(Path.home())
-    pywin_path = (home_path + '\\Ikomia\\Python\\lib\\site-packages\\win32')
-    if pywin_path not in sys.path:
-        sys.path.append(pywin_path)
-
-    pywin_path = (home_path + '\\Ikomia\\Python\\lib\\site-packages\\win32\\lib')
-    if pywin_path not in sys.path:
-        sys.path.append(pywin_path)
-
-    pywin_path = (home_path + '\\Ikomia\\Python\\lib\\site-packages\\Pythonwin')
-    if pywin_path not in sys.path:
-        sys.path.append(pywin_path)
 
 # Your imports below
 import torch
